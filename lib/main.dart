f41d3c515038ad45539335c266ed5e662b87a8f2
Wade <wadehuang36@gmail.com>
2018-09-05 15:19:46 -0400

    update splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:emergency_nav_lights_sim/page/splash-page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new EnlApp());
-
-class EnlApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Emergency NAV Ligths SIMULATOR',
-      home: new SplashPage(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: 'Emergency NAV Ligths SIMULATOR',
+    home: SplashPage(),
+  ));
 }
--- lib/page/splash-page.dart
+++ lib/page/splash-page.dart
@@ -11,12 +11,7 @@ class SplashPage extends StatefulWidget {
 
 class SplashPageState extends State<SplashPage> {
   startTime() async {
-    return new Timer(Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => HomePage()),
-      );
-    });
+    return new Timer(Duration(seconds: 2), navigateToHome);
   }
 
   @override
@@ -25,23 +20,38 @@ class SplashPageState extends State<SplashPage> {
     startTime();
   }
 
+  bool isNav = false;
+  void navigateToHome(){
+    if (isNav) return;
+
+    isNav = true;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => HomePage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: InkWell(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Container(
+              margin: EdgeInsets.all(12.0),
               child: Text("EMERGENCY\nNAV LIGHTS\nSIMULATOR",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 32.0, fontFamily: "AzoSansUber")),
             ),
+            Image.asset("assets/images/boat.png"),
             Container(
-                child: Image.asset("assets/images/boat.png")),
+                margin: EdgeInsets.all(32.0),
+                child: Text("build by SWARM", style: TextStyle(color: Color.fromARGB(255, 155, 155, 155)),  textAlign: TextAlign.center)),
           ],
         ),
+        onTap: navigateToHome,
       ),
     );
   }
