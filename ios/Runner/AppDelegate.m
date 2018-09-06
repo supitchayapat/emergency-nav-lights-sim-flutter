adea41686ec770fe993d8eaf0ba0af90962ba06c
SWARM <SWARM@SWARMs-MacBook-Pro-7.local>
2018-09-06 14:18:28 -0400

    add change brightness on ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,15 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+  self.window.screen.brightness = 1.0;
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
